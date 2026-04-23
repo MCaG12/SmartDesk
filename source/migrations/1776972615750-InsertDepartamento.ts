20298b90a5704a9f5d63d3ef8624c95b4d3daa6c
Murilo Carvalho <MuriloCarvalho2583@Outlook.com>
2026-04-23 17:15:55 -0300

    FEATURE: CREATES MIGRATIONS TO POPLUATE NECESSARY DATABASE

---
+++ source/migrations/1776972615750-InsertDepartamento.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InsertDepartamento1776972615750 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `INSERT INTO "DEPARTAMENTO" VALUES ('RECURSOS HUMANOS'), ('SUPORTE TECNICO'), ('ATENDIMENTO AO CLIENTE'), ('PROGRAMACAO');`
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
